401a1754b61c233a6b8f80b135fba545a3825a12
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2021-10-11 23:15:26 +0530

    Update custom.js

--- Lecture-01/js/custom.js
+++ Lecture-01/js/custom.js
@@ -25,4 +25,12 @@ $(document).ready(function(){
                 }
                 });
                 $(window).scroll(function() {
-                        )
+                        if ($(window).scrollTop() > 400) {
+                                $("#navigation").css("background-color","#0EB493");
+                        }else {
+                                $("#navigation").css("background-color","rgb(16, 22, 54, 0.2)");
+                        }
+                
+                
+                });
+        
